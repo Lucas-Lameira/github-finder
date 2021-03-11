3c4dbf8d54b070f0436ed1b2e49190e562a069f9
Lucas de souza lameira <lucasslameira@gmail.com>
2021-03-11 14:01:19 -0300

    project is done but not deployd yet, im going to change styles

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -20,6 +20,19 @@ import {
   SET_LOADING,  
  } from '../types';
 
+
+let githubClientId;
+let githubClientSecret;
+
+//checking if the project is in production or not
+if(process.env.NODE_ENV !== 'production'){
+  githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+}else {
+  githubClientId = process.env.GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
+}
+
 //create initial state/ githubstate includes all the actions
 const GithubState = props => {
   //initial global state relative to github
@@ -35,8 +48,8 @@ const GithubState = props => {
   //search user
   async function searchUsers (text) {
     setIsLoading();
-    const response = await axios.get(`https://api.github.com/search/users?q=${text}`);
-    
+    const response = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${githubClientId}&client_secret=${githubClientSecret}`);
+      
     dispatch({
       type: SEARCH_USERS,
       payload: response.data.items //data u wanna send
@@ -46,7 +59,7 @@ const GithubState = props => {
    //get a single github user
    async function getUser (username) {    
     setIsLoading();    
-    const response = await axios.get(`https://api.github.com/users/${username}`);
+    const response = await axios.get(`https://api.github.com/users/${username}?client_id=${githubClientId}&client_secret=${githubClientSecret}`);
     
     dispatch({
       type: GET_USER,
@@ -57,8 +70,9 @@ const GithubState = props => {
   //get a bunch of repos from user
   async function getUserRepos (username) {
     setIsLoading();
-    const response = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc`);
-    
+    const response = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${githubClientId}&client_secret=${githubClientSecret}`);
+
+
     dispatch({
       type: GET_REPOS,
       payload: response.data
